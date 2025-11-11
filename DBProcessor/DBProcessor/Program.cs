237cc4b3acb2440e25748a09512b3dca3d950cd3
Felipe Assis <felipe.assis.2002@gmail.com>
2025-11-11 00:16:40 -0300

    Ignoring secrets

--- DBProcessor/DBProcessor/Program.cs
+++ DBProcessor/DBProcessor/Program.cs
@@ -1,22 +1,20 @@
 ﻿using DBProcessor.Data_Classes;
 using DBProcessor.Database;
+using DBProcessor.Secrets;
 
 public class Program
 {
-    private const string DB_PATH = "mongodb+srv://nelsonhilariaokria:uonQ8xiBByKLtKq7@kria.zzp95.mongodb.net/?retryWrites=true&w=majority&appName=Kria";
-
-    private const string DB_TABLE = "Candidato";
-    private const string DB_COLLECTION = "TabTransacoes";
+    
     
     public static async Task Main(string[] args)
     {
-        DatabaseWrapper dbWrapper = new DatabaseWrapper(DB_PATH);
+        DatabaseWrapper dbWrapper = new DatabaseWrapper(Secrets.DB_PATH);
 
         await dbWrapper.ConnectRaw();
 
         Console.WriteLine("Inicializando conexão com o servidor");
 
-        var data = await dbWrapper.ConnectToDB<ApplicantData>(DB_TABLE, DB_COLLECTION);
+        var data = await dbWrapper.ConnectToDB<ApplicantData>(Secrets.DB_TABLE, Secrets.DB_COLLECTION);
 
         Console.WriteLine("🚀 Iniciando processamento...");
 
